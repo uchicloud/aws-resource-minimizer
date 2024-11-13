54d349899ef5abdbd1b387db6a9756d2177e42b8
Uchida.S <uchida.s@cp-info.co.jp>
2024-11-14 03:09:47 +0900

    DIngチャットへの送信を実装

--- index.ts
+++ index.ts
@@ -1,6 +1,43 @@
 import type { Handler } from "aws-lambda";
+import crypto from 'crypto';
+
+const secret = process.env.DING_SECRET ?? '';
+const endpoint = process.env.DING_ENDPOINT ?? '';
+
+const calcHmac = (time: number) => {
+    const sign = `${time}\n${secret}`;
+    const hmac = crypto.createHmac('sha256', secret);
+    hmac.update(sign);
+    const digest = hmac.digest('base64');
+    return encodeURIComponent(digest);
+}
 
 export const handler: Handler = async (event, context) => {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+    const content = JSON.stringify(event);
+    const now = Date.now();
+    const hmac = calcHmac(now);
+    const url = `${endpoint}&timestamp=${now}&sign=${hmac}`;
+    const message = {
+        "msgtype": "text",
+        "text": {
+            content
+        }
+    };
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'charset': 'utf-8',
+        },
+        body: JSON.stringify(message)
+    });
+
+    if (!res.ok) {
+        throw new Error(`Failed to send message: ${res.statusText}`);
+    }
+    const json = await res.json();
+    console.log('RESPONSE: \n' + JSON.stringify(json, null, 2));
     return context.logStreamName;
 }
