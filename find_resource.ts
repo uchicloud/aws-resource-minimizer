ff4b91c57f7817814bcb733eca875eca901b3bc9
Uchida.S <uchida.s@cp-info.co.jp>
2024-11-19 03:00:09 +0900

    日付処理のバリエーション追加

--- find_resource.ts
+++ find_resource.ts
@@ -3,7 +3,9 @@ import { __Client, ResourceExplorer2Client, SearchCommand, type Resource, type S
 
 type ResourceDict = {
     emptyTag: Resource[],
-    remove: Resource[]
+    remove: Resource[],
+    over: Resource[],
+    error: Resource[],
 }
 
 const client = new ResourceExplorer2Client({
@@ -13,25 +15,29 @@ const client = new ResourceExplorer2Client({
 export const countResources = async (params: SearchCommandInput): Promise<ResourceDict> => {
     const searchCommand = new SearchCommand(params);
     const thisMonth = getThisMonth();
+    const yyyyMM = `${thisMonth.getFullYear()}${(thisMonth.getMonth() + 1).toString().padStart(2, '0')}`;
+     
+    console.log(thisMonth, yyyyMM);
 
     let res;
     const result: ResourceDict = 
-        {'emptyTag': [], 'remove': []};
+        {'emptyTag': [], 'remove': [], 'over': [], 'error': []};
     do {
         res = await client.send(searchCommand);
         res.Resources?.filter((r) => r.Properties?.some((p) => Array.from(p.Data).every((obj) => obj.Key === 'Name'))).forEach((r) => result.emptyTag.push(r));
-        res.Resources?.filter((r) => r.Properties?.some((p) => Array.from(p.Data).some((obj) => obj.Key.indexOf(thisMonth) === 0))).forEach((r) => result.remove.push(r));
+        res.Resources?.filter((r) => r.Properties?.some((p) => Array.from(p.Data).some((obj) => obj.Key.indexOf(yyyyMM) === 0))).forEach((r) => result.remove.push(r));
+        res.Resources?.filter((r) => r.Properties?.some((p) => Array.from(p.Data).some((obj) => obj.Key < yyyyMM && obj.Key < `${yyyyMM}01`))).forEach((r) => result.over.push(r));
         params.NextToken = res.NextToken;
     } while (res.NextToken);
 
     return result;
 }
 
-export const getThisMonth = (): string => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-    return `${year}${month.toString().padStart(2, '0')}`;
+export const getThisMonth = (): Date => {
+    const now = new Date();
+    now.setMonth(now.getMonth() + 1);
+    now.setDate(0);
+    return now;
 }
 
 countResources({ QueryString: 'resourcetype:ec2:instance' }).then(r => console.dir(r, {depth: 6})).catch(console.error);
