2dd3d66a47326b199e052ef2d7424db13312e9ae
Uchida.S <uchida.s@cp-info.co.jp>
2024-11-29 20:37:26 +0900

    Nameタグがないリソースを考慮

--- find_resource.ts
+++ find_resource.ts
@@ -18,13 +18,18 @@ export const categorizeResources = async (params: SearchCommandInput, thisMonth:
         res = await client.send(searchCommand);
         res.Resources?.forEach((r) => 
             r.Properties?.forEach((p) => {
-                if ((p.Data as { [K: string]: string }[]).every((obj) => ignoreTags.includes(obj.Key))) {
+                const d = (p.Data as { [K: string]: string }[]) ?? [];
+                if (d.map(obj => obj.Key).every(key => key !== 'Name')) {
+                    const Value = r.Arn?.split(':').slice(-1)[0] ?? 'Unknown';
+                    d.push({ Key: 'Name', Value });
+                }
+                if (d.every((obj) => ignoreTags.includes(obj.Key))) {
                     result.emptyTag.push(r);
-                } else if ((p.Data as { [K: string]: string }[]).some((obj) => obj.Key.indexOf(yyyyMM) === 0)) {
+                } else if (d.some((obj) => obj.Key.indexOf(yyyyMM) === 0)) {
                     result.remove.push(r);
-                } else if ((p.Data as { [K: string]: string }[]).some((obj) => !isValidDate(obj))) {
+                } else if (d.some((obj) => !isValidDate(obj))) {
                     result.error.push(r);
-                } else if ((p.Data as { [K: string]: string }[]).some((obj) => isBeforeThisMonth(obj, thisMonth))) {
+                } else if (d.some((obj) => isBeforeThisMonth(obj, thisMonth))) {
                     result.over.push(r);
                 }
             })
